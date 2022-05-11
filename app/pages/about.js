3e4a0f611b568bd5e688a7c635769dc6dc6fd465
Keiler Swanson <keilerswansonwebdev@gmail.com>
2022-05-11 14:37:16 -0400

    Recompose AboutMosaic into About page

--- app/pages/about.js
+++ app/pages/about.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
 import NavTop from '../components/NavTop'
-import AboutMosaic from '../components/AboutMosaic'
-import styles from '../styles/AboutPage.module.scss'
+import Image from 'next/image'
+import dadSmile from '../public/dad-smile.jpeg'
+import dadWork from '../public/dad-working.jpeg'
+// import AboutMosaic from '../components/AboutMosaic'
+import styles from '../styles/About.module.scss'
 
 export default function AboutPage() {
 	return (
-		<div className={styles.aboutPage} >
+		<div className={styles.about}>
 			<Head>
         <title>About | Swanson Furniture</title>
         <meta charSet='utf-8' />
@@ -15,7 +18,30 @@ export default function AboutPage() {
 				<link rel='icon' href='/favicon.ico' />
       </Head>
 			<NavTop />
-			<AboutMosaic />
+			{/* <AboutMosaic /> */}
+			<div className={styles.mosaic}>
+				<div className={styles.image}>
+					<Image 
+						src={dadWork}
+						alt='Scott Swanson hand-planing a table'	
+						layout='fill'
+						objectFit='cover'
+						priority
+					/>
+				</div>
+				<div className={styles.image}>
+					<Image 
+						src={dadSmile}
+						alt='Scott Swanson smiling'	
+						layout='fill'
+						objectFit='cover'
+						priority
+					/>
+				</div>
+				<p className={styles.copy}>
+					
+				</p>
+			</div>
 		</div>
 	)
 }
